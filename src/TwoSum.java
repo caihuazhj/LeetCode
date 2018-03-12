5bbca35b249bdffd8506ebe541e8b5f0e9ce6936
caihuazhj <zj765570468@gmail.com>
2018-03-13 00:20:59 +0800

    ec

---
+++ src/TwoSum.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimerTask;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ * @author Administrator
+ *
+ */
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int[] twoSumByHashMap(int[] nums, int target) {
+		Map<Integer, Integer> maps = new HashMap<>();
+		int[] res = new int[2];
+		for(int i = 0;i<nums.length;i++){
+			maps.put(nums[i], i);
+		}
+		for(int i = 0;i<nums.length;i++){
+			int temp = target - nums[i];
+			if (maps.containsValue(temp) && maps.get(temp) != i) {
+					res[0] = i;
+					res[1] = maps.get(temp);
+					return res;
+			}
+		}
+		
+		throw new IllegalArgumentException("No two sum solution");
+	}
+ public static int[]  twoSum(int[] nums, int target) {
+
+            int[] res = new int[2];
+            for (int i=0;i<nums.length;i++){
+                for (int j=i+1;j<nums.length;j++){
+                    if(nums[i]+nums[j] == target){
+                        res[0] = i;
+                        res[1] = j;
+                    }
+                }
+            }
+
+            return  res;
+    }
+
+}
