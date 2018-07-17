1a6e376832e26cbde2dad84603e125dad52772c8
caihuazhj <zj765570468@gmail.com>
2018-07-17 16:32:32 +0800

    二叉树镜像

---
+++ src/Tree/Mirror.java
@@ -0,0 +1,38 @@
+package Tree;
+
+/**
+ * 求二叉树的镜像
+ * 核心思想：递归，调换左右子树的值
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mirror {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> mirrorRecursively(TreeNode root){
+        if (root ==null){
+            return res;
+        }
+        if (root.left==null&&root.right==null){
+            res.add(root.val);
+            return res;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        if (root.left!=null){
+            mirrorRecursively(root.left);
+        }
+        if (root.right!=null){
+            mirrorRecursively(root.right);
+        }
+    }
+}
